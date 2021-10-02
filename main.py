7cc1dc10f5d9006544f6b09ff80371c61ef1f509
Shiho <shihohohohohohoho@gmail.com>
2021-10-02 21:21:32 +0800

    ismael kalbo

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-print('hello world')
-print('kalbo isma321321321321el')
-print("shit")
+import pygame  
+pygame.init()  
+white = (255, 255, 255)  
+window = pygame.display.set_mode((400, 400))  
+  
+pygame.display.set_caption('Gmame Testing')  
+  
+image = pygame.image.load(r'resources\test.png')  
+  
+# infinite loop   
+while True:  
+    window.fill((255, 255, 255))
+    window.blit(image, (0, 0))  
+  
+    for event in pygame.event.get():  
+        if event.type == pygame.QUIT:  
+            pygame.quit()  
+            quit()  
+        pygame.display.update()   
