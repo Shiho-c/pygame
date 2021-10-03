2300d32a9b401a8d415fc3d2c259b90af528cb60
ryansantos0625 <89053269+ryansantos0625@users.noreply.github.com>
2021-10-03 08:45:29 -0700

    123
    no  changes made. Tried basics of Classes.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from pygame import rect 
 from pygame.locals import *
 
-from resources.LabelText import LabelText
+from LabelText import LabelText
 #import random
 pygame.init()  
 white = (255, 255, 255)  
@@ -10,15 +10,15 @@ black = (0, 0, 0)
 window = pygame.display.set_mode((400, 400))  
 pygame.display.set_caption('Game Testing')  
 #load images
-#image = pygame.image.load('resources/test.png')  
+#image = pygame.image.load('resources/ocean.jpg')
 
-corbel_font = pygame.font.SysFont('Corbel',35) 
+corbel_font = pygame.font.SysFont('Corbel',35)
   
 start_button = LabelText("Start", corbel_font, black, window.get_width() / 2, window.get_height() / 2 - 50 )
 quit_button = LabelText("Quit", corbel_font, black, window.get_width() / 2, start_button.get_y() + start_button.get_height() )
 # infinite loop  
 run = True 
-while run:  
+while run:
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:  
             run = False  
@@ -29,10 +29,11 @@ while run:
             elif quit_button.collision(cursor):
                 run = False
 
-    pygame.display.update()  
+    #pygame.display.update()  
     window.fill((255, 255, 255))
     window.blit(start_button.surface(), (start_button.get_x(), start_button.get_y()))
     window.blit(quit_button.surface(), (quit_button.get_x(), quit_button.get_y()))
-   
+    pygame.display.update()
+  
 pygame.quit()          
 
