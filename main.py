4e0b0f35daee0f5e1da829b16fb1ca46274eb298
ryansantos0625 <89053269+ryansantos0625@users.noreply.github.com>
2021-10-03 00:41:56 -0700

    Test Update - Window working and is put into infinite loop not until closed.
    Updated Import Code chunk, and Tested different image files (jpg. and PNG.). Also removed quit( ) line and replace it with boolean, ( line 12 anmd line 20

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import pygame  
+import pygame 
+from pygame.locals import *
+#import random
 pygame.init()  
 white = (255, 255, 255)  
 window = pygame.display.set_mode((400, 400))  
+pygame.display.set_caption('Game Testing')  
+#load images
+image = pygame.image.load('resources/test.png')  
   
-pygame.display.set_caption('Gmame Testing')  
-  
-image = pygame.image.load(r'resources\test.png')  
-  
-# infinite loop   
-while True:  
+# infinite loop  
+run = True 
+while run:  
+     
     window.fill((255, 255, 255))
-    window.blit(image, (0, 0))  
+    window.blit(image, (0, 0)) 
   
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:  
-            pygame.quit()  
-            quit()  
-        pygame.display.update()   
+            run = False  
+        pygame.display.update()     
+pygame.quit()             
