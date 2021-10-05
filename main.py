8f648f42d53124bca8b9af858f2854ee99683c2c
Shiho <63734164+Shiho-c@users.noreply.github.com>
2021-10-05 13:12:34 +0800

    added options button with hover effect

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ pygame.mixer.music.play()
 corbel_font = pygame.font.SysFont('Corbel',35)
   
 start_button = LabelText("Start", corbel_font, black, window.get_width() / 2, window.get_height() / 2 - 50 )
-quit_button = LabelText("Quit", corbel_font, black, window.get_width() / 2, start_button.get_y() + start_button.get_height() + 50 )
+option_button = LabelText("Option", corbel_font, black, window.get_width() / 2, start_button.get_y() + start_button.get_height() + 50 )
+quit_button = LabelText("Quit", corbel_font, black, window.get_width() / 2, option_button.get_y() + option_button.get_height() + 50 )
 player = pygame.image.load('resources/player.png')
 player = pygame.transform.scale(player, (50, 50))
 playerX, playerY = 0, 0
@@ -35,12 +36,11 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if start_button.collision(cursor):
                 background = (0, 0, 0)
+                pygame.mixer.music.stop()
                 game_screen = True
-                pygame.mixer.music.stop()
             elif quit_button.collision(cursor):
-                run = False
                 pygame.mixer.music.stop()
-    key = pygame.key.get_pressed()
+                run = False
         
     if playerX + player.get_width() >= window_width:
         playerX -= movementSpeed
@@ -51,6 +51,7 @@ while run:
     elif playerY < 1:
         playerY += movementSpeed
     else:
+        key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
             playerX -= movementSpeed
         if key[pygame.K_RIGHT]:
@@ -62,7 +63,6 @@ while run:
         if event.type == pygame.QUIT:  
             run = False  
 
-    print(str(playerY))
 
     if game_screen:
         window.blit(game_screenBackground, game_screenBackground.get_rect())
@@ -73,12 +73,16 @@ while run:
             start_button.change_color((255,0, 0))
         elif quit_button.collision(cursor):
             quit_button.change_color((255,0, 0))
+        elif option_button.collision(cursor):
+            option_button.change_color((255,0, 0))
         
         else:
             start_button.change_color((0,0, 0))
+            option_button.change_color((0,0, 0))
             quit_button.change_color((0,0, 0))
         window.fill(background)
         window.blit(start_button.surface(), (start_button.get_x(), start_button.get_y()))
+        window.blit(option_button.surface(), (option_button.get_x(), option_button.get_y()))
         window.blit(quit_button.surface(), (quit_button.get_x(), quit_button.get_y()))
     clock.tick(60)
     pygame.display.update()
