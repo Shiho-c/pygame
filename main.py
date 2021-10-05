3e4ce52151132f9c5395d38b384fa1a7d816a1b1
Shiho <shihohohohohohoho@gmail.com>
2021-10-05 10:04:28 +0800

    start button is now working

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ black = (0, 0, 0)
 window = pygame.display.set_mode((400, 400))  
 pygame.display.set_caption('Game Testing')  
 #load images
-#image = pygame.image.load('resources/test.png')  
+#image = pygame.image.load('resources/ocean.jpg')
 
-corbel_font = pygame.font.SysFont('Corbel',35) 
+corbel_font = pygame.font.SysFont('Corbel',35)
   
 start_button = LabelText("Start", corbel_font, black, window.get_width() / 2, window.get_height() / 2 - 50 )
 quit_button = LabelText("Quit", corbel_font, black, window.get_width() / 2, start_button.get_y() + start_button.get_height() )
 # infinite loop  
 run = True 
-while run:  
+while run:
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:  
             run = False  
@@ -33,6 +33,7 @@ while run:
     window.fill(background)
     window.blit(start_button.surface(), (start_button.get_x(), start_button.get_y()))
     window.blit(quit_button.surface(), (quit_button.get_x(), quit_button.get_y()))
-   
+    pygame.display.update()
+  
 pygame.quit()          
 
