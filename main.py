57100c0285cfcfcf2537f3ac3ff70b3beb16043d
MumeiOW <91926953+MumeiOW@users.noreply.github.com>
2021-10-05 11:14:12 +0800

    sad
    tite

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ while run:
     window.blit(quit_button.surface(), (quit_button.get_x(), quit_button.get_y()))
     if game_screen:
         pygame.draw.rect(window, (0, 0, 128), player)
-    clock.tick(69)
+    clock.tick(70)
     pygame.display.update()
   
 pygame.quit()          
